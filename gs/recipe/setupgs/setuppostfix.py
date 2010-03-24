9c65d2d39b6aef69f27d2efedb890527d81c1b04
Michael JasonSmith <mpj17@onlinegroups.net>
2010-03-25 08:46:59 +1100

    Adding a postfix config generator, and calling the code.

--- gs/recipe/setupgs/setuppostfix.py
+++ gs/recipe/setupgs/setuppostfix.py
@@ -7,20 +7,16 @@ import stat
 import Products.XWFMailingListManager
 
 class SetupPostfix(object):
-    def __init__(self, postfixConfigPath, postfixUser, 
-                    postfixUserGroup, domainName):
-        self.__postfixConfigPath = postfixConfigPath
-        self.__postfixUser = postfixUser
-        self.__postfixUserGroup = postfixUserGroup
+    def __init__(self, domainName):
         self.__pathToGroupServer = None
-        self.__domainName = domainName
+        self.domainName = domainName
     
     @property
     def pathToGroupServer(self):
         if self.__pathToGroupServer == None:
             self.__pathToGroupServer = '.'
         return self.__pathToGroupServer
-            
+    
     def copy_utils(self):
         utilsName = 'utils'
         scriptName = 'smtp2zope*py'
@@ -39,4 +35,32 @@ class SetupPostfix(object):
             stat.S_IRGRP | stat.S_IXGRP
         for util in glob(join(utilsDest, scriptName)):
             chmod(util, perms)
+    
+    def create_postfix_config(self):
+        configDest = join(self.pathToGroupServer, 'postfix_config')
+        mkdir(configDest)
+        
+        subs = {
+            'gsPath': self.pathToGroupServer,
+            'domain': self.domainName}
+            
+        verifyAddress = 'verify-address:   '\
+            '"|%(gsPath)s/utils/smtp2zope-nonautomatic.py '\
+            'http://%(domain)/acl_users/verify_address"\n' % subs
+        groupAutomatic = 'group-automagic:  '\
+            '"|%(gsPath)s/utils/smtp2zope.py  '\
+            'http://%(domain)s/ListManager"\n' % subs
+        fileName = join(configDest, 'groupserver.aliases')
+        gsAliases = file(fileName, 'w')
+        gsAliases.write(verifyAddress)
+        gsAliases.write(groupAutomatic)
+        gsAliases.close()
+        
+        gsVirtualContents = '%(domain)s\tvirtual\n'\
+            'verify@%(domain)s\tverify-address\n'\
+            '@%(domain)s\tgroup-automagic\n'
+        fileName = join(configDest, 'groupserver.virtual')
+        gsVirtual = file(fileName, 'w')
+        gsVirtual.write(gsVirtualContents)
+        gsVirtual.close()
 
