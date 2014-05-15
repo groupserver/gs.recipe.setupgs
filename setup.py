63c96efe59504e60ff9894edeb7f0c3a7773f391
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-15 13:52:47 -0700

    Updating the metadata.
    --HG--
    rename : docs/CHANGES.txt => docs/HISTORY.txt

--- setup.py
+++ setup.py
@@ -15,20 +15,18 @@
 """
 This module contains the tool of gs.recipe.setupgs
 """
+import codecs
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
 version = get_version()
 
-long_description = (
-    open('README.txt').read()
-    + '\n' +
-    open(os.path.join('docs', 'CONTRIBUTORS.txt')).read()
-    + '\n' +
-    open(os.path.join('docs', 'CHANGES.txt')).read()
-    + '\n'
-    )
+with codecs.open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.txt"), encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
 entry_point = 'gs.recipe.setupgs:Recipe'
 entry_points = {"zc.buildout": ["default = %s" % entry_point]}
 
@@ -41,12 +39,13 @@ setup(name='gs.recipe.setupgs',
       classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Framework :: Buildout',
+        'Framework :: Zope2',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: Zope Public License',
         "Natural Language :: English",
-        "Operating System :: POSIX :: Linux"
+        "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
@@ -56,10 +55,12 @@ setup(name='gs.recipe.setupgs',
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         ],
-      keywords='zope groupserver recipe setup instance',
-      author='Michael JasonSmith',
-      author_email='mpj17@onlinegroups.net',
-      url='',
+      keywords='zope, groupserver, recipe, setup, instance, install',
+      author='Richard Waid',
+      author_email='richard@onlinegroups.net',
+      maintainer='Michael JasonSmith',
+      maintainer_email='mpj17@onlinegroups.net',
+      url='https://source.iopen.net/groupserver/gs.recipe.setupgs/',
       license='ZPL 2.1',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['gs', 'gs.recipe'],
