894544603361a5192efdd46884dd4382678b3a72
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-15 14:03:25 -0700

    Adding docstrings, and updating the test for failure

--- gs/recipe/setupgs/tests/scriptcreation.py
+++ gs/recipe/setupgs/tests/scriptcreation.py
@@ -19,11 +19,14 @@ from shutil import rmtree
 from tempfile import mkdtemp
 from unittest import TestCase, main as unittest_main
 import mock
+import zc.buildout
 import gs.recipe.setupgs
 from gs.recipe.setupgs import Recipe
 
 
 class TestScriptCreation(TestCase):
+    'Test the creation of the script.'
+
     def setUp(self):
         self.tempdir = mkdtemp()
         self.bindir = '{0}/bin'.format(self.tempdir)
@@ -64,7 +67,7 @@ class TestScriptCreation(TestCase):
         rmtree(self.tempdir)
 
     def should_run_false_options(self, val):
-        '''Test if seting the "run-opnce" option to false always causes
+        '''Test if seting the "run-once" option to false always causes
         the should_run method to return True.'''
         self.recipe.options = {'run-once': val}
         with open(self.recipe.fileName, 'w') as togglefile:
@@ -73,14 +76,17 @@ class TestScriptCreation(TestCase):
         self.assertTrue(r)
 
     def test_should_run_false(self):
+        'Test if setting run-once to False prevents the script from being run'
         self.should_run_false_options('false')
         self.should_run_false_options('False')
 
     def test_should_run_no(self):
+        'Test if setting run-once to No prevents the script from being run'
         self.should_run_false_options('no')
         self.should_run_false_options('No')
 
     def test_should_run_off(self):
+        'Test if setting run-once to Off prevents the script from being run'
         self.should_run_false_options('off')
         self.should_run_false_options('Off')
 
@@ -97,27 +103,32 @@ class TestScriptCreation(TestCase):
         self.assertFalse(r)
 
     def test_quote_command(self):
+        'Test the quote_command function.'
         c = 'I am a fish'.split()
         expected = '"I" "am" "a" "fish"'
         r = self.recipe.quote_command(c)
         self.assertEqual(expected, r)
 
     def test_mark_locked(self):
+        'Test if we mark_locked creates a lockfile'
         self.recipe.mark_locked()
         r = exists(self.recipe.fileName)
         self.assertTrue(r)
 
     def test_create_script(self):
+        'Test the creation of a script.'
         r = self.recipe.create_script()
         expected = '# -*- coding: utf-8 -*-'
         self.assertEqual(expected, r[:23])
 
     def test_create_script_options(self):
+        'Test if some of the options are in the script.'
         r = self.recipe.create_script()
         self.assertIn('example.com', r)
         self.assertIn('SetupGS', r)
 
     def test_write_script(self):
+        'Test if the write_script function works.'
         outText = 'I am a fish'
         filename = self.recipe.write_script(outText)
         with open(filename, 'r') as infile:
@@ -125,6 +136,7 @@ class TestScriptCreation(TestCase):
         self.assertEqual(outText, inText)
 
     def test_get_script(self):
+        'Test the get_script functiom.'
         filename = self.recipe.get_script()
         with open(filename, 'r') as infile:
             inText = infile.read()
@@ -132,12 +144,14 @@ class TestScriptCreation(TestCase):
         self.assertEqual(expected, inText[:23])
 
     def test_get_script_command(self):
+        'Test the get_script_command function'
         r = self.recipe.get_script_command()
         self.assertIn('instance', r)
         self.assertIn('run', r)
         self.assertIn(self.tempdir, r)
 
     def test_install_success(self):
+        'Test if we get a 0 if the install function works.'
         # Note that 0 is success for a command in the Unix shell
         gs.recipe.setupgs.subprocess.call = mock.MagicMock(return_value=0)
         t = self.recipe.install()
@@ -147,14 +161,12 @@ class TestScriptCreation(TestCase):
         self.assertEqual(t, tuple())
 
     def test_install_fail(self):
+        'Test that we get a UserError is raised if the install function fails.'
         # Note that 1 is failure for a command in the Unix shell
         gs.recipe.setupgs.sys.exit = mock.MagicMock()
         gs.recipe.setupgs.subprocess.call = mock.MagicMock(return_value=1)
-        self.recipe.install()
+        self.assertRaises(zc.buildout.UserError, self.recipe.install)
         self.assertEqual(1, gs.recipe.setupgs.subprocess.call.call_count)
-        gs.recipe.setupgs.sys.exit.assert_called_with(1)
-        args, kw_args = gs.recipe.setupgs.sys.stderr.write.call_args
-        self.assertIn('Issue running', args[0])
 
 if __name__ == '__main__':
     unittest_main()
