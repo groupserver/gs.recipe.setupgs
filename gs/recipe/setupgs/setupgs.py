0ea5ae33efc488bc30359aa1ed0d6016c42afbcb
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-26 10:07:33 -0700

    Minor refactor.

--- gs/recipe/setupgs/setupgs.py
+++ gs/recipe/setupgs/setupgs.py
@@ -15,27 +15,32 @@
 from Testing import makerequest
 from AccessControl.SecurityManagement import newSecurityManager
 from zc.buildout import UserError
-from Products.GroupServer.creation import manage_addGroupserverSite
+from Products.GroupServer import creation
 SITE_ID = 'initial_site'
 
 
 class SetupGS(object):
     """ Setup GroupServer."""
     def __init__(self, app, zope_admin_name=''):
+        self.set_admin_security_manager(app, zope_admin_name)
+        self.app = makerequest.makerequest(app)
+
+    def set_admin_security_manager(self, app, zope_admin_name=''):
+        'Dark magic to do with security'
         admin = app.acl_users.acl_users.getUserById(zope_admin_name)
         admin = admin.__of__(app.acl_users)
         newSecurityManager(None, admin)
-        self.app = makerequest.makerequest(app)
 
     def create_site(self, siteId, title, supportEmail,
         admin_email, admin_password, canonicalHost, canonicalPort,
         smtp_host, smtp_port, smtp_user, smtp_password):
         '''Create a GroupServer site'''
 
-        manage_addGroupserverSite(self.app, siteId, title, supportEmail,
-            admin_email, admin_password, canonicalHost, canonicalPort,
+        creation.manage_addGroupserverSite(self.app, siteId, title,
+            supportEmail, admin_email, admin_password,
+            canonicalHost, canonicalPort,
             smtp_host, smtp_port, smtp_user, smtp_password)
-        # TODO: turn the asserts into tests that raise zc.buildout.UserError
+
         if not hasattr(self.app, siteId):
             m = '"{0}" folder not found'.format(siteId)
             raise UserError(m)
