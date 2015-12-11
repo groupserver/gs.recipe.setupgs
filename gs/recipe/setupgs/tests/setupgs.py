05394a62a67fe0c2bc9f66ef344bc1690a7e9113
Michael JasonSmith <mpj17@onlinegroups.net>
2015-12-11 15:19:42 -0800

    Adding a Copyright block

--- gs/recipe/setupgs/tests/setupgs.py
+++ gs/recipe/setupgs/tests/setupgs.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2014, 2015 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import absolute_import, unicode_literals, print_function
 from mock import patch, MagicMock  # lint:ok
 from unittest import TestCase
 #from OFS.Application import Application
